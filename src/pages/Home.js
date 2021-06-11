47af287617b812fe2fdd579687d604db25a8a911
samuel <timeuss.sam@gmail.com>
2021-06-11 23:55:11 +0200

    chore(test): test jcc poke api request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,35 @@
 /* eslint-disable space-before-function-paren */
-/* eslint-disable react/prop-types */
 import React from 'react'
-// import { useState } from 'react'
+// import axios from 'axios'
 
-import axios from 'axios'
+import pokemon from 'pokemontcgsdk'
 
-// let UrlState = {
-//   next: '',
-//   prev: ''
-// }
+pokemon.configure({ apiKey: '1bc96399-f62e-4230-98e4-f7ad9d51212b' })
 
-// const [state, setSate] = useState(UrlState)
-
-const fetchByAll = async () => {
-  const { data } = await axios(
-    `https://pokeapi.co/api/v2/pokemon?offset=40&limit=20`
-  )
-  console.log(data)
-  return data
+const getAllSet = async () => {
+  pokemon.set.find('').then((set) => {
+    console.log(set)
+  })
 }
 
-const fetchPrevious = async () => {
-  const url = await fetchByAll()
-  const previousUrl = url.previous
-  console.log(previousUrl)
-  const { data } = await axios(`${previousUrl}`)
-  console.log('les data davant : ', data)
-  return data
+const getCard = async () => {
+  pokemon.card.find('base1-4').then((card) => {
+    console.log(card)
+  })
 }
 
-const fetchNext = async () => {
-  const url = await fetchByAll()
-  const nextUrl = url.next
-  console.log(nextUrl)
-  const { data } = await axios(`${nextUrl}`)
-  console.log('les data apres : ', data)
-  return data
-}
-
-const MyButton = (props) => {
-  const { buttonTitle, func } = props
-  return <button onClick={func}> {buttonTitle}</button>
+const filterByQ = async () => {
+  pokemon.card.where({ q: 'name:blastoise' }).then((result) => {
+    console.log(result.data[0].name) // "Blastoise"
+  })
 }
 
 const Home = () => {
-  // const [state, setSate] = useState(UrlState)
-
   return (
-    <div>
-      <MyButton buttonTitle="previous" func={fetchPrevious} />
-      <MyButton buttonTitle="start" func={fetchByAll} />
-      <MyButton buttonTitle="next" func={fetchNext} />
+    <div className="d-flex flex-column">
+      <button onClick={getAllSet}> Get all set</button>
+      <button onClick={getCard}> Get a single card by ID</button>
+      <button onClick={filterByQ}> Filter cards via the q parameter</button>
     </div>
   )
 }
