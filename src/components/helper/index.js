88aa6c3243f3a413b5158001529e507ac99f699e
samuel <timeuss.sam@gmail.com>
2021-06-21 00:36:22 +0200

    style(detail card): test style for card composition

---
+++ src/components/helper/index.js
@@ -0,0 +1,46 @@
+export const stringToIcon = (string) => {
+  switch (string) {
+    case 'Colorless':
+      break
+    case 'Bug':
+      break
+    case 'Dark':
+      break
+    case 'Dragon':
+      break
+    case 'Electric':
+      break
+    case 'Fairy':
+      break
+    case 'Fighting':
+      break
+    case 'Fire':
+      break
+    case 'Flying':
+      break
+    case 'Ghost':
+      break
+    case 'Grass':
+      break
+    case 'Ground':
+      break
+    case 'Ice':
+      break
+    case 'Normal':
+      break
+    case 'Poison':
+      break
+    case 'Psychic':
+      break
+    case 'Rock':
+      break
+    case 'Steel':
+      break
+    case 'Water':
+      break
+    default:
+      break
+  }
+}
+
+export default stringToIcon
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -6,6 +6,8 @@ import Context from '../components/context/'
 import pokemon from 'pokemontcgsdk'
 pokemon.configure({ apiKey: '1bc96399-f62e-4230-98e4-f7ad9d51212b' })
 
+import Image from 'react-bootstrap/Image'
+
 import MyNavBar from '../components/navbar'
 
 const Details = () => {
@@ -16,38 +18,98 @@ const Details = () => {
     const fetchData = async () => {
       pokemon.card.find(`${cardDetail}`).then((card) => {
         setData({ myData: [card] })
-        console.log(card)
       })
     }
     fetchData()
-  }, [])
+  }, [setData])
 
   console.log(data.myData)
+
+  // const stringToIcon = (string) => {
+
+  // }
 
   const MyCard = () => {
     return data.myData ? (
       data.myData.map((item, index) => {
         return (
-          <div key={index} className="d-flex">
-            <div>
-              <img src={item.images.large} alt="poke card" className="w-75" />
+          <div key={index} className="container mt-5">
+            <div className="border border-info my-5">
+              <div className="row">
+                <div className="col-12 col-md-3">
+                  <Image
+                    src={item.images.small}
+                    fluid
+                    alt="poke card"
+                    className="shadow"
+                  />
+                </div>
+                <div className="col">
+                  <div className="row">
+                    <div className="col-8">
+                      <h1>{item.name}</h1>
+                      <p>
+                        {item.supertype} - {item.subtypes}
+                      </p>
+                    </div>
+                    <div className="col-4 text-end">HP {item.hp}</div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div>
-              <p>name: {item.name}</p>
-              <p>artist: {item.artist}</p>
-              <p>id : {item.id}</p>
-              <p>National Pokedex Numbers : {item.nationalPokedexNumbers}</p>
-              <p>number: {item.number}</p>
-              <p>rarity: {item.rarity}</p>
-              <p>subtypes: {item.subtypes}</p>
-              <p> retreat cost: {item.convertedRetreatCost}</p>
-              <p>retreat cost type: {item.retreatCost}</p>
-              <p>evolves to : {item.evolvesTo}</p>
-              <p>supertype: {item.supertype}</p>
-              <p>subtypes: {item.subtypes}</p>
-              <p>type: {item.types}</p>
-              <p>evolves from: {item.evolvesFrom}</p>
-              <p>rules : {item.rules}</p>
+            <div className="d-flex justify-content-between container">
+              <div>
+                <img src={item.images.small} alt="poke card" />
+              </div>
+
+              <div>
+                <p>name: {item.name}</p>
+                <p>artist: {item.artist}</p>
+                <p>id : {item.id}</p>
+                <p>National Pokedex Numbers : {item.nationalPokedexNumbers}</p>
+                <p>number: {item.number}</p>
+                <p>rarity: {item.rarity}</p>
+                <p> retreat cost: {item.convertedRetreatCost}</p>
+                <p>retreat cost type: {item.retreatCost}</p>
+              </div>
+              <div>
+                <div>
+                  evolves to :{' '}
+                  <ul>
+                    {item.evolvesTo || item.evolvesTo === !undefined
+                      ? item.evolvesTo.map((item, index) => {
+                          return <li key={index}>{item}</li>
+                        })
+                      : 'no'}
+                  </ul>
+                </div>
+                <p>supertype: {item.supertype}</p>
+                <p>subtypes: {item.subtypes}</p>
+                <p>type: {item.types}</p>
+                <p>
+                  evolves from:{' '}
+                  {item.evolvesFrom || item.evolvesFrom === !undefined
+                    ? item.evolvesFrom
+                    : 'no'}
+                </p>
+                <p>
+                  rules :{' '}
+                  {item.rules ? item.rules : 'no specific rule for this card'}
+                </p>
+              </div>
+            </div>
+            <div className="bg-light container">
+              <p>attacks:</p>
+              {item.attacks.map((item, index) => {
+                return (
+                  <div key={index} className="shadow p-2">
+                    <p>{item.name}</p>
+                    <p>damage: {item.damage}</p>
+                    <p>{item.text}</p>
+                    <p>{item.cost}</p>
+                  </div>
+                )
+              })}
             </div>
           </div>
         )
