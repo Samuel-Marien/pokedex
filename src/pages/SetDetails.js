0ebfe16f07340b797adb1590947636bda0cd76df
samuel <timeuss.sam@gmail.com>
2021-07-08 15:57:15 +0200

    feat(set details): add menu to sort result(only view)

--- src/pages/SetDetails.js
+++ src/pages/SetDetails.js
@@ -1,24 +1,40 @@
 /* eslint-disable space-before-function-paren */
 import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import Context from '../components/context/'
 
 import pokemon from 'pokemontcgsdk'
 pokemon.configure({ apiKey: '1bc96399-f62e-4230-98e4-f7ad9d51212b' })
 
+import Dropdown from 'react-bootstrap/Dropdown'
+
 import MyNavBar from '../components/navbar'
 
-import Dropdown from 'react-bootstrap/Dropdown'
+const MyDropItem = (props) => {
+  const { func, href, name } = props
+  return (
+    <Dropdown.Item onClick={func} href={href}>
+      {name}
+    </Dropdown.Item>
+  )
+}
+
+MyDropItem.propTypes = {
+  func: PropTypes.func,
+  href: PropTypes.string,
+  name: PropTypes.string
+}
 
 const SetDetails = () => {
   const { setDetail } = useContext(Context)
   const { setCardDetail } = useContext(Context)
   const [data, setData] = useState('')
   const [title, setTitle] = useState('')
-  const [cardOrList, setCardOrList] = useState(false)
-
-  let dropTitle = cardOrList ? 'Images' : 'List'
+  const [myDropViewTitle, setMyDropViewTitle] = useState('')
+  const [myDropTitle, setMyDropTitle] = useState('')
+  const [myDropOrderTitle, setMyDropOrderTitle] = useState('')
 
   console.log(setDetail)
 
@@ -46,6 +62,7 @@ const SetDetails = () => {
         <p className="ms-2">({setDetail})</p>
       </div>
       <div className="container d-flex align-items-baseline">
+        {/* First dropDown  */}
         <p className="me-2">View as</p>
         <Dropdown>
           <Dropdown.Toggle
@@ -54,28 +71,69 @@ const SetDetails = () => {
             id="dropdown-basic"
             size="sm"
           >
-            {dropTitle}
+            {myDropViewTitle}
             <Dropdown.Menu>
-              <Dropdown.Item
-                onClick={() => {
-                  setCardOrList(true)
+              <MyDropItem
+                func={() => {
+                  setMyDropViewTitle('Images')
                 }}
                 href="#/action-1"
-              >
-                Images
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() => {
-                  setCardOrList(false)
+                name="Images"
+              />
+              <MyDropItem
+                func={() => {
+                  setMyDropViewTitle('List')
                 }}
                 href="#/action-2"
-              >
-                List
-              </Dropdown.Item>
+                name="List"
+              />
             </Dropdown.Menu>
           </Dropdown.Toggle>
         </Dropdown>
+        {/* Second dropDown  */}
         <p className="ms-4 me-2">Sorted by</p>
+        <Dropdown as="ButtonGroup">
+          <Dropdown.Toggle
+            className="px-2"
+            variant="outline-info"
+            id="dropdown-basic2"
+            size="sm"
+          >
+            {myDropTitle}
+            <Dropdown.Menu>
+              <MyDropItem
+                func={() => {
+                  setMyDropTitle('Set/Number')
+                }}
+                href="#/action-11"
+                name="Set/Number"
+              />
+              <MyDropItem
+                func={() => {
+                  setMyDropTitle('Name')
+                }}
+                href="#/action-22"
+                name="Name"
+              />
+              <MyDropItem
+                func={() => {
+                  setMyDropTitle('Release Date')
+                }}
+                href="#/action-33"
+                name="Release Date"
+              />
+              <MyDropItem
+                func={() => {
+                  setMyDropTitle('Rarity')
+                }}
+                href="#/action-44"
+                name="Rarity"
+              />
+            </Dropdown.Menu>
+          </Dropdown.Toggle>
+        </Dropdown>
+        {/* Third dropDown  */}
+        <p className="me-2 ms-4">Ordered by</p>
         <Dropdown>
           <Dropdown.Toggle
             className="px-2"
@@ -83,24 +141,22 @@ const SetDetails = () => {
             id="dropdown-basic"
             size="sm"
           >
-            {dropTitle}
+            {myDropOrderTitle}
             <Dropdown.Menu>
-              <Dropdown.Item
-                onClick={() => {
-                  setCardOrList(true)
+              <MyDropItem
+                func={() => {
+                  setMyDropOrderTitle('Asc')
                 }}
                 href="#/action-1"
-              >
-                Images
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() => {
-                  setCardOrList(false)
+                name="Asc"
+              />
+              <MyDropItem
+                func={() => {
+                  setMyDropOrderTitle('Desc')
                 }}
                 href="#/action-2"
-              >
-                List
-              </Dropdown.Item>
+                name="Desc"
+              />
             </Dropdown.Menu>
           </Dropdown.Toggle>
         </Dropdown>
