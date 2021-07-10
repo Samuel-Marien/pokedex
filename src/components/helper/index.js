4c66931501d0af6ac1e414a3affe93c1924f82ae
samuel <timeuss.sam@gmail.com>
2021-07-11 00:22:03 +0200

    featcard detail): zoom on card & set and fix type icon

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -1,62 +1,29 @@
 import React from 'react'
-import { MdStars } from 'react-icons/md'
-
-import { ReactComponent as Bug } from '../../images/Pokémon_Bug_Type_Icon.svg'
-import { ReactComponent as Dark } from '../../images/Pokémon_Dark_Type_Icon.svg'
-import { ReactComponent as Dragon } from '../../images/Pokémon_Dragon_Type_Icon.svg'
-import { ReactComponent as Electric } from '../../images/Pokémon_Electric_Type_Icon.svg'
-import { ReactComponent as Fairy } from '../../images/Pokémon_Fairy_Type_Icon.svg'
-import { ReactComponent as Fighting } from '../../images/Pokémon_Fighting_Type_Icon.svg'
-import { ReactComponent as Fire } from '../../images/Pokémon_Fire_Type_Icon.svg'
-import { ReactComponent as Flying } from '../../images/Pokémon_Flying_Type_Icon.svg'
-import { ReactComponent as Ghost } from '../../images/Pokémon_Ghost_Type_Icon.svg'
-import { ReactComponent as Grass } from '../../images/Pokémon_Grass_Type_Icon.svg'
-import { ReactComponent as Ground } from '../../images/Pokémon_Ground_Type_Icon.svg'
-import { ReactComponent as Ice } from '../../images/Pokémon_Ice_Type_Icon.svg'
-import { ReactComponent as Poison } from '../../images/Pokémon_Poison_Type_Icon.svg'
-import { ReactComponent as Psychic } from '../../images/Pokémon_Psychic_Type_Icon.svg'
-import { ReactComponent as Rock } from '../../images/Pokémon_Rock_Type_Icon.svg'
-import { ReactComponent as Steel } from '../../images/Pokémon_Steel_Type_Icon.svg'
-import { ReactComponent as Water } from '../../images/Pokémon_Water_Type_Icon.svg'
 
 const stringToIcon = (string) => {
   switch (string) {
-    case 'Bug':
-      return <Bug style={{ width: '25px' }} />
     case 'Darkness':
-      return <Dark style={{ width: '25px' }} />
+      return <img src="/images/darkness.png" style={{ width: '25px' }} />
     case 'Dragon':
-      return <Dragon style={{ width: '25px' }} />
+      return <img src="/images/dragon.png" style={{ width: '25px' }} />
     case 'Lightning':
-      return <Electric style={{ width: '25px' }} />
+      return <img src="/images/electric.png" style={{ width: '25px' }} />
     case 'Fairy':
-      return <Fairy style={{ width: '25px' }} />
+      return <img src="/images/fairy.png" style={{ width: '25px' }} />
     case 'Fighting':
-      return <Fighting style={{ width: '25px' }} />
+      return <img src="/images/fighting.png" style={{ width: '25px' }} />
     case 'Fire':
-      return <Fire style={{ width: '25px' }} />
-    case 'Flying':
-      return <Flying style={{ width: '25px' }} />
-    case 'Ghost':
-      return <Ghost style={{ width: '25px' }} />
+      return <img src="/images/fire.png" style={{ width: '25px' }} />
     case 'Grass':
-      return <Grass style={{ width: '25px' }} />
-    case 'Ground':
-      return <Ground style={{ width: '25px' }} />
-    case 'Ice':
-      return <Ice style={{ width: '25px' }} />
-    case 'Poison':
-      return <Poison style={{ width: '25px' }} />
+      return <img src="/images/grass.png" style={{ width: '25px' }} />
     case 'Psychic':
-      return <Psychic style={{ width: '25px' }} />
-    case 'Rock':
-      return <Rock style={{ width: '25px' }} />
+      return <img src="/images/psy.png" style={{ width: '25px' }} />
     case 'Metal':
-      return <Steel style={{ width: '25px' }} />
+      return <img src="/images/metal.png" style={{ width: '25px' }} />
     case 'Water':
-      return <Water style={{ width: '25px' }} />
+      return <img src="/images/water.png" style={{ width: '25px' }} />
     case 'Colorless':
-      return <MdStars size={27} color="#6c757d" />
+      return <img src="/images/colorless.png" style={{ width: '25px' }} />
     default:
       console.log('error dude!')
       break
